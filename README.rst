d01db7beb1bf54763718b5987c789ae161cf0a0f
Ivar Lazzaro <ivarlazzaro@gmail.com>
2014-09-10 19:52:03 -0700

    Refinements before publishin on pypi

---
+++ README.rst
@@ -0,0 +1,22 @@
+=======
+Apicapi
+=======
+
+Provides an interface for the APIC rest API.
+
+Install
+=======
+
+Using pip::
+
+    pip install apicapi
+
+easy_install::
+
+    easty_install apicapi
+
+manually::
+
+    git clone https://github.com/noironetworks/apicapi.git
+    cd apicapi
+    python setup.py install
--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
-# Copyright (c) 2014 Cisco Systems
-# All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-#
-# @author: Ivar Lazzaro (ivar-lazzaro), Cisco Systems Inc.
-
-import os
 from setuptools import setup
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "apicapi",
@@ -31,5 +10,4 @@ setup(
     url = "http://github.com/noironetworks/apicapi/",
     license = "http://www.apache.org/licenses/LICENSE-2.0",
     description = "This library provides an interface to the APIC REST api.",
-    long_description=read('README.md'),
 )
