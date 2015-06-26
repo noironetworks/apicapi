d1d11482d31611889560b5f65a74da34b1b99793
Amit Bose <bose@noironetworks.com>
2015-06-26 11:37:19 -0700

    Bump version to 1.0.5
    Signed-off-by: Amit Bose <bose@noironetworks.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import setuptools
 
 setuptools.setup(
     name="apicapi",
-    version="1.0.4.dev",
+    version="1.0.5",
     zip_safe=False,
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
