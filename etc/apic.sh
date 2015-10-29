72fb3c4a1d103f5bd0c5232bfe70e7d5854422e1
Mandeep Dhami <dhami@noironetworks.com>
2015-10-28 21:58:02 -0700

    Added useful aliases for diag/support

---
+++ etc/apic.sh
@@ -0,0 +1,31 @@
+pcat() {
+  for i in "$@"
+  do
+    python -mjson.tool "$i"
+  done
+}
+
+opflex-eps() {
+  for i in /var/lib/opflex-agent-ovs/endpoints/*.ep ; do
+    echo '#----------------------------------------'
+    echo '# EP: ' `basename $i .ep`
+    pcat $i;
+    echo ""
+  done
+}
+
+opflex-db() {
+  gbp_inspect -fprq DmtreeRoot
+}
+
+opflex-flows() {
+  ovs-ofctl -OOpenFlow13 dump-flows br-int "$@" ;
+}
+
+ovs-ports() {
+  ovs-ofctl show br-int | awk '/addr:/ {print $2, $1}'
+}
+
+vxlan-pkts() {
+  tcpdump -lane -i bond0.4093 "$@" port 8472 | grep --line-buffered -v OTV ;
+}
