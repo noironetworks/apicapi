6df5ae2f7d4ce899074973485704ce3f127cd156
Mandeep Dhami <dhami@noironetworks.com>
2017-03-25 12:46:20 -0700

    Updated unit tests for conf.d usage

--- apicapi/tests/unit/common/test_apic_common.py
+++ apicapi/tests/unit/common/test_apic_common.py
@@ -244,6 +244,7 @@ class ConfigMixin(object):
         # Configure global option apic_system_id
         cfg.CONF.set_override('apic_system_id', APIC_SYSTEM_ID)
         cfg.CONF.set_override('config_file', 'etc/conf_sample.ini')
+        cfg.CONF.set_override('config_file', [])
 
         # Configure the Cisco APIC mechanism driver
         apic_test_config = {
