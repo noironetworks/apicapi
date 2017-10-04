1f4e8786cdc921f1ed12d3136436f112bc031034
Thomas Bachman <tbachman@yahoo.com>
2017-10-04 12:12:47 +0000

    Bump version to 1.0.17

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 
 setuptools.setup(
     name="apicapi",
-    version="1.0.16",
+    version="1.0.17",
     zip_safe=False,
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
