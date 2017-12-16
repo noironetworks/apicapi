8e4a88e6a917f3dedecabef22c585a079a3973c2
Thomas Bachman <tbachman@yahoo.com>
2017-12-16 00:38:34 +0000

    Bump version to 1.1.0
    We should be bumping the minor version for releases, and not
    the patch version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 
 setuptools.setup(
     name="apicapi",
-    version="1.0.18",
+    version="1.1.0",
     zip_safe=False,
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
