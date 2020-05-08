88ecc76bb31e357d0e01dc8f76e7f4b723381be1
Thomas Bachman <tbachman@yahoo.com>
2020-05-08 20:15:15 +0000

    Bump version to 1.6.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 
 setuptools.setup(
     name="apicapi",
-    version="1.6.5",
+    version="1.6.6",
     zip_safe=False,
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
