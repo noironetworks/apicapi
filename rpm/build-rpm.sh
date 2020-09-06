dd4beb74e877feee4c3cfb83b2cc436de554c285
Thomas Bachman <tbachman@yahoo.com>
2020-09-06 11:20:13 +0000

    Support python2 builds for RHEL7/8
    Update package names in order to build packages that support both
    RHEL7 and RHEL8 for python2.

--- rpm/build-rpm.sh
+++ rpm/build-rpm.sh
@@ -4,8 +4,8 @@
 BUILD_DIR=${BUILD_DIR:-`pwd`/rpmbuild}
 mkdir -p $BUILD_DIR/BUILD $BUILD_DIR/SOURCES $BUILD_DIR/SPECS $BUILD_DIR/RPMS $BUILD_DIR/SRPMS
 RELEASE=${RELEASE:-1}
-VERSION=`python setup.py --version`
+VERSION=`python2 setup.py --version`
 SPEC_FILE=apicapi.spec
 sed -e "s/@VERSION@/$VERSION/" -e "s/@RELEASE@/$RELEASE/" rpm/$SPEC_FILE.in > $BUILD_DIR/SPECS/$SPEC_FILE
-python setup.py sdist --dist-dir $BUILD_DIR/SOURCES
+python2 setup.py sdist --dist-dir $BUILD_DIR/SOURCES
 rpmbuild --clean -ba --define "_topdir $BUILD_DIR" $BUILD_DIR/SPECS/$SPEC_FILE
